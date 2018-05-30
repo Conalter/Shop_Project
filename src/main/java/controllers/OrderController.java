344adf99ff777e25618813e0f07a7954834cd044
omikron15 <omikron15@hotmail.co.uk>
2018-05-30 11:31:11 +0100

    refactors OrderController to improve login functionality

--- src/main/java/controllers/OrderController.java
+++ src/main/java/controllers/OrderController.java
@@ -22,12 +22,15 @@ public class OrderController {
 
         get("/orders", (req,res) -> {
             Map<String, Object> model = new HashMap<>();
+
             String loggedInUser = LoginController.getLoggedInUsername(req,res);
-            model.put("user", loggedInUser);
             boolean isLoggedIn = LoginController.isLoggedIn(req,res);
+            model.put("user", loggedInUser);
             model.put("isLoggedIn", isLoggedIn);
+
             List<Order> orders = DBHelper.getAll(Order.class);
             model.put("orders", orders);
+
             model.put("template", "templates/orders/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
